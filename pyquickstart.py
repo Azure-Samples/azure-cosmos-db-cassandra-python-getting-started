aa8f7e32264c7625cf3cd8a87181e82892427576
Sneha Gunda <sngun@microsoft.com>
2020-08-13 17:23:19 -0700

    Updating insert statements

--- pyquickstart.py
+++ pyquickstart.py
@@ -14,7 +14,7 @@ def PrintTable(rows):
     t = PrettyTable(['UserID', 'Name', 'City'])
     for r in rows:
         t.add_row([r.user_id, r.user_name, r.user_bcity])
-    print t
+    print (t)
 
 ssl_opts = {
             'ca_certs': DEFAULT_CA_BUNDLE_PATH,
@@ -30,27 +30,25 @@ cluster = Cluster([cfg.config['contactPoint']], port = cfg.config['port'], auth_
 )
 session = cluster.connect()
 
-print "\nCreating Keyspace"
+print ("\nCreating Keyspace")
 session.execute('CREATE KEYSPACE IF NOT EXISTS uprofile WITH replication = {\'class\': \'NetworkTopologyStrategy\', \'datacenter\' : \'1\' }');
-print "\nCreating Table"
+print ("\nCreating Table")
 session.execute('CREATE TABLE IF NOT EXISTS uprofile.user (user_id int PRIMARY KEY, user_name text, user_bcity text)');
 
-insert_data = session.prepare("INSERT INTO  uprofile.user  (user_id, user_name , user_bcity) VALUES (?,?,?)")
-session.execute(insert_data, [1,'Lybkov','Seattle'])
-session.execute(insert_data, [2,'Doniv','Dubai'])
-session.execute(insert_data, [3,'Keviv','Chennai'])
-session.execute(insert_data, [4,'Ehtevs','Pune'])
-session.execute(insert_data, [5,'Dnivog','Belgaum'])
-session.execute(insert_data, [6,'Ateegk','Narewadi'])
-session.execute(insert_data, [7,'KannabbuS','Yamkanmardi'])
+session.execute("INSERT INTO  uprofile.user  (user_id, user_name , user_bcity) VALUES (%s,%s,%s)", [1,'Lybkov','Seattle'])
+session.execute("INSERT INTO  uprofile.user  (user_id, user_name , user_bcity) VALUES (%s,%s,%s)", [2,'Doniv','Dubai'])
+session.execute("INSERT INTO  uprofile.user  (user_id, user_name , user_bcity) VALUES (%s,%s,%s)", [3,'Keviv','Chennai'])
+session.execute("INSERT INTO  uprofile.user  (user_id, user_name , user_bcity) VALUES (%s,%s,%s)", [4,'Ehtevs','Pune'])
+session.execute("INSERT INTO  uprofile.user  (user_id, user_name , user_bcity) VALUES (%s,%s,%s)", [5,'Dnivog','Belgaum'])
+session.execute("INSERT INTO  uprofile.user  (user_id, user_name , user_bcity) VALUES (%s,%s,%s)", [6,'Ateegk','Narewadi'])
+session.execute("INSERT INTO  uprofile.user  (user_id, user_name , user_bcity) VALUES (%s,%s,%s)", [7,'KannabbuS','Yamkanmardi'])
 
-
-print "\nSelecting All"
+print ("\nSelecting All")
 rows = session.execute('SELECT * FROM uprofile.user')
 PrintTable(rows)
 
  
-print "\nSelecting Id=1"
+print ("\nSelecting Id=1")
 rows = session.execute('SELECT * FROM uprofile.user where user_id=1')
 PrintTable(rows)
 
