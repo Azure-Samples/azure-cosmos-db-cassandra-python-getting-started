8f3017d00c288543ca3eba31f9def779765209b7
IriaO <iriaosara@microsoft.com>
2022-09-21 12:04:35 -0400

    running pylint

--- CosmosRetryPolicy.py
+++ CosmosRetryPolicy.py
@@ -1,8 +1,7 @@
+"""
+TODO Will update after testing
+"""
 import logging
-import re
-from time import sleep
-import warnings
-
 from cassandra.policies import RetryPolicy
 
 from cassandra import ConsistencyLevel, WriteType
@@ -19,57 +18,66 @@ class CosmosRetryPolicy(RetryPolicy):
     readTimeoutRetriesEnabled = None
     writeTimeoutRetriesEnabled = None
 
-    # TODO: document func after testing
-
-
     def get_fixed_backoff_time_in_millis(self):
+        """
+        TODO Will update after testing
+        """
         return self.fixedBackOffTimeInMillis
-        
-    
+
     def get_growing_backoff_time_in_millis(self):
+        """
+        TODO Will update after testing
+        """
         return self.growingBackOffTimeInMillis
 
-    
     def get_max_retry_count(self):
+        """
+        TODO Will update after testing
+        """
         return self.maxRetryCount
 
     def is_read_timeout_retries_enabled(self):
+        """
+        TODO Will update after testing
+        """
         return self.readTimeoutRetriesEnabled
 
     def is_write_timeout_retries_enabled(self):
+        """
+        TODO Will update after testing
+        """
         return self.writeTimeoutRetriesEnabled
 
-
-
-    def on_read_timeout(self, query, consistency, required_responses,
-                        received_responses, data_retrieved, retryCount):
-                        
-                        if retryCount != 0:
-                            return RetryPolicy.RETHROW, None
-                        elif received_responses >= required_responses and not data_retrieved:
-                            return self.maxRetryCount, consistency==ConsistencyLevel
-                        else:
-                            return RetryPolicy.RETHROW, None 
-
-
-    def on_write_timeout(self, writeTimeoutRetriesEnabled, retryCount, write_type, consistency ):
-        if writeTimeoutRetriesEnabled == True:
+    def on_read_timeout(self, query, consistency, required_responses,received_responses, data_retrieved, retry_count):
+        """
+        TODO Will update after testing
+        """
+        if retry_count != 0:
             return RetryPolicy.RETHROW, None
-        elif retryCount==0 and write_type==WriteType.BATCH_LOG:
-            RetryPolicy.RETRY, consistency==ConsistencyLevel
+        elif received_responses >= required_responses and not data_retrieved:
+            return self.maxRetryCount, consistency==ConsistencyLevel
         else:
-            return RetryPolicy.RETHROW
+            return RetryPolicy.RETHROW, None
 
-    def retry_many_times_or_throw(self, retryCount):
+    def on_write_timeout(self,writeTimeoutRetriesEnabled,retry_count,write_type,consistency):
+        """
+        TODO Will update after testing
+        """
+        if writeTimeoutRetriesEnabled is False:
+            return RetryPolicy.RETHROW, None
+        elif retry_count==0 and write_type==WriteType.BATCH_LOG:
+            RetryPolicy.RETRY, consistency==ConsistencyLevel
+
+    def retry_many_times_or_throw(self, retry_count):
         """ generated source for method retryManyTimesOrThrow """
-        return RetryPolicy.RETRY if self.maxRetryCount == -1 or retryCount < self.maxRetryCount else RetryPolicy.RETHROW
+        return RetryPolicy.RETRY if self.maxRetryCount == -1 or retry_count < self.maxRetryCount else RetryPolicy.RETHROW
 
-    def on_unavailable(self,consistency,retryCount):
-        """
-        """
-        # TODO to be added after testing read and writetout
-        return (self.RETRY_NEXT_HOST, None) if retryCount == 0 else (RetryPolicy.RETHROW, None)
+    # def on_unavailable(self,consistency,retryCount):
+    #     """
+    #     """
+    #     return (self.RETRY_NEXT_HOST, None) if retryCount == 0 else (RetryPolicy.RETHROW, None)
 
-    def get_retry_after_millis():
-        # TODO 
-        return
+    # def get_retry_after_millis():
+    #     """
+    #     """
+    #     return None
